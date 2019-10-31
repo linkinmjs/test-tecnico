95b20176c6d6be2bfce884b477949986ed0fba8b
Mauricio <linkin_mjs@gmail.com>
2019-10-30 21:14:46 -0300

    añadido favicon

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+  <link rel="shortcut icon" href="{{ asset('img/image5093.png') }}">
   <!-- A modo de test se colocan las librerias booststrap de esta forma mas no es aconsejable ya que disminuye el rendimiento de carga del website al consumin servicios externos -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
